4662763501965765ceaba50c6ef3f89cc3c0a163
Adeonir Kohl <adeonir@gmail.com>
2022-02-23 16:43:05 -0300

    Update button to render as another element

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -1,20 +1,28 @@
-import { ButtonHTMLAttributes, ReactNode } from 'react'
+import { ElementType, ReactNode } from 'react'
 import cn from 'classnames'
 
 type Props = {
   children: ReactNode
   className?: string
-} & ButtonHTMLAttributes<HTMLButtonElement>
+  as?: ElementType
+}
 
-export const Button = ({ className, children, ...props }: Props) => {
+export const Button = ({
+  children,
+  className,
+  as = 'button',
+  ...props
+}: Props) => {
+  const Tag = `${as}` as keyof JSX.IntrinsicElements
+
   return (
-    <button
+    <Tag
       {...props}
       className={cn(
-        `rounded-lg bg-pink-500 px-4 py-2 font-bold text-slate-100 outline-pink-300 transition hover:bg-pink-550 focus:outline focus:outline-2 ${className}`
+        `inline-block rounded-lg bg-pink-500 px-4 py-2 font-bold text-slate-100 outline-pink-300 transition hover:bg-pink-550 focus:outline focus:outline-2 ${className}`
       )}
     >
       {children}
-    </button>
+    </Tag>
   )
 }
