6c5d9b945b8afba7b36b6635f7d1a70e185bad6b
Adeonir Kohl <adeonir@gmail.com>
2022-02-24 17:15:26 -0300

    Small responsiveness fixes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-gray-800">Welcome to Remix</h1>
+      <h1 className="text-3xl font-bold text-slate-100">Welcome to Remix</h1>
     </div>
   )
 }
