7bc596652ce633cd04126d2134d164c00611771c
Adeonir Kohl <adeonir@gmail.com>
2022-02-25 14:55:55 -0300

    Update ui components styles

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Button = ({ children, className, ...props }: Props) => {
   const classNames = cn(
-    'inline-block rounded-lg bg-teal-500 px-4 py-2 font-bold text-slate-100 outline-teal-300 transition hover:bg-teal-550 focus:outline disabled:cursor-not-allowed disabled:opacity-75',
+    'inline-block rounded-lg bg-teal-500 px-4 py-2 font-bold text-slate-100 transition hover:bg-teal-550 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500 disabled:cursor-not-allowed disabled:opacity-75',
     className
   )
 
--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Input = ({ className, ...props }: Props) => {
   const classNames = cn(
-    'rounded-lg bg-slate-400 px-4 py-2 text-slate-800 outline-teal-300 placeholder:text-slate-100 focus:outline w-full disabled:opacity-75 disabled:cursor-not-allowed',
+    'w-full rounded-lg bg-slate-400 px-4 py-2 text-slate-800 placeholder:text-slate-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500 disabled:cursor-not-allowed disabled:opacity-75',
     className
   )
 
