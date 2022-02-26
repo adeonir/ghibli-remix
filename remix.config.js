7902152d1acf4f0d38419f93a85499e095febdab
Adeonir Kohl <adeonir@gmail.com>
2022-02-26 01:04:34 -0300

    Add vercel deploy config

--- remix.config.js
+++ remix.config.js
@@ -3,9 +3,6 @@
  */
 module.exports = {
   ignoredRouteFiles: ['.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
 }
