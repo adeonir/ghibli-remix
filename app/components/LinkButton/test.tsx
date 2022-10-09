55166c71f93e3ad87a56ed9b12adbef6db7147fd
Hugo Lima Romão <hugo8romao@gmail.com>
2022-10-09 09:59:23 -0400

    Add test to LinkButton component

---
+++ app/components/LinkButton/test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { LinkButton } from '.'
+
+describe('<Input />', () => {
+  it(`should render correctly`, () => {
+    const { container } = render(
+      <LinkButton to="pathname">Children text</LinkButton>,
+      {
+        wrapper: MemoryRouter,
+      }
+    )
+
+    expect(
+      screen.getByRole('link', { name: /Children text/i })
+    ).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should pass the "to" prop correctly', () => {
+    render(<LinkButton to="/pathname">Children text</LinkButton>, {
+      wrapper: MemoryRouter,
+    })
+
+    expect(
+      screen.getByRole('link', { name: /Children text/i })
+    ).toHaveAttribute('href', '/pathname')
+  })
+})
