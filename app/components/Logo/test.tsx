bf672036ccedef878c3be3d97c98ec33be974d76
Hugo Lima Romão <hugo8romao@gmail.com>
2022-10-09 10:02:51 -0400

    Add test to Logo component

---
+++ app/components/Logo/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { GhibliLogo } from '.'
+
+describe('<Input />', () => {
+  it(`should render correctly`, () => {
+    const { container } = render(<GhibliLogo />, {
+      wrapper: MemoryRouter,
+    })
+
+    expect(screen.getByLabelText(/Ghibli Logo/i)).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+})
